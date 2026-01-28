a7e5ac98d46ea96e78c0f6c3a62b98ca92051883
Khushi <khushichauhan2506@gmail.com>
2026-01-28 19:52:41 +0530

    Header done

--- components/Headers.tsx
+++ components/Headers.tsx
@@ -1,21 +1,11 @@
-// const Header = () => {
-//     return (
-//         <div>Header</div>
-//     )
-// }
-//
-// export default Header
-//   //20:17
-
-
 import Link from "next/link";
 import Image from "next/image";
-// import NavItems from "@/components/NavItems";
-// import UserDropdown from "@/components/UserDropdown";
-// import {searchStocks} from "@/lib/actions/finnhub.actions";
+import NavItems from "@/components/NavItems";
+import UserDropdown from "@/components/UserDropdown";
+import {searchStocks} from "@/lib/actions/finnhub.actions";
 
-const Header = ( ) => {
-    // const initialStocks = await searchStocks();
+const Header = async ({ user }: { user: User }) => {
+    const initialStocks = await searchStocks();
 
     return (
         <header className="sticky top-0 header">
@@ -23,14 +13,11 @@ const Header = ( ) => {
                 <Link href="/">
                     <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
                 </Link>
-
                 <nav className="hidden sm:block">
-                    {/*<NavItems initialStocks={initialStocks} />*/}
+                    <NavItems initialStocks={initialStocks} />
                 </nav>
 
-                {/*<UserDropdown user={user} initialStocks={initialStocks} />*/}
-
-
+                <UserDropdown user={user} initialStocks={initialStocks} />
             </div>
         </header>
     )
