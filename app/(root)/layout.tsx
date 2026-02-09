31add0a009d1dbbcf1359174fd98d57c35740fab
Khushi <khushichauhan2506@gmail.com>
2026-02-09 22:12:24 +0530

    layout changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,21 +1,28 @@
 
 import Header from "@/components/Headers";
+import {auth} from "@/lib/better-auth/auth";
+import {headers} from "next/headers";
+import {redirect} from "next/navigation";
 
 const Layout = async ({ children }: { children : React.ReactNode }) => {
-    // const session = await auth.api.getSession({ headers: await headers() });
+    const session = await auth.api.getSession({ headers: await headers() });
+
+    if(!session?.user) redirect('/sign-in');
+
+    const user = {
+        id: session.user.id,
+        name: session.user.name,
+        email: session.user.email,
+    }
 
     return (
-        <html lang="en">
-        <body>
         <main className="min-h-screen text-gray-400">
-            <Header />
+            <Header user={user} />
+
             <div className="container py-10">
                 {children}
             </div>
         </main>
-
-        </body>
-        </html>
     )
 }
 export default Layout
